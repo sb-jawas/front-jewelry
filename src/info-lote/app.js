a3ccae76d9ce9f209c3fa30e45f724edc82c16bd
bhamidou <badrhamidou@gmail.com>
2023-11-27 18:06:25 +0100

    show buttons only if the lote is the viewer user

--- src/info-lote/app.js
+++ src/info-lote/app.js
@@ -3,7 +3,7 @@ import { domain, asyncApiRequest, formatDate } from "../utils/funcs.js";
 const table = document.getElementById("table");
 const tblBody = document.getElementById("tbody");
 
-const addComponent = document.getElementById("add-component");
+
 const btnClasificar = document.getElementById("btn-clasificar");
 const btnTarde = document.getElementById("btn-tarde");
 const btnRechazar = document.getElementById("btn-rechazar");
@@ -13,6 +13,8 @@ const asignCheck = document.getElementsByName("asign");
 let ubi = document.getElementById("ubi")
 let status = document.getElementById("status")
 let infoLote = document.getElementById("info-lote")
+let observation = document.getElementById("observation")
+let trTable = document.getElementById("trTable")
 
 
 let i = 1;
@@ -25,65 +27,86 @@ let idLote = localStorage.getItem("loteId")
 url = domain+"/api/lote/"+idLote
 asyncApiRequest("GET",url).then(function(lote){
     infoLote.textContent = "Lote: " + lote.id
-    ubi.textContent += " "+ lote.ubi 
-    status.textContent += " "+  lote.status
+    ubi.textContent = "Ubicación: "+ lote.ubi 
+    status.textContent = "Estado: "+  lote.status
+    observation.textContent = "Observación: "+  lote.observation
+
+    let userLocal = localStorage.getItem("userId")
+    if(lote.user_id == userLocal){   
+        let btnAdd = document.createElement("i")
+        btnAdd.setAttribute("class","bi bi-clipboard-plus-fill btn btn-success")
+        btnAdd.id = "add-component"
+        let th = document.createElement("th")
+        th.appendChild(btnAdd)
+        trTable.appendChild(th)
+        let addComponent = document.getElementById("add-component");
+        listenerBtnAdd(addComponent)
+    }else{
+        let buttons = document.getElementsByName("btn")
+        for (var i = 0, len = buttons.length; i != len; ++i) {
+            buttons[0].parentNode.removeChild(buttons[0]);
+        }
+    }
 })
 
 
-addComponent.addEventListener("click", function () {
+
+function listenerBtnAdd(addComponent){
+    addComponent.addEventListener("click", function () {
     
-    let lista =  null
-    if(i>2){
-        lista = localStorage.getItem("lista-componentes")
-        console.log(localStorage.getItem("lista-componentes"))
-    }else{
-
-        lista = createList(componentes)
-    }
+        let lista =  null
+        if(i>2){
+            lista = localStorage.getItem("lista-componentes")
+            console.log(localStorage.getItem("lista-componentes"))
+        }else{
     
-
-  let inputCantidad = document.createElement("input");
-  inputCantidad.setAttribute("type", "number");
-
-  let btnDelete = document.createElement("button");
-  btnDelete.setAttribute("class", "btn btn-danger");
-  btnDelete.setAttribute("name", "btnDelete");
-
-
-  let icon = document.createElement("i");
-  icon.setAttribute("class", "bi bi-trash3-fill");
-  icon.setAttribute("name", "icoDelete");
-
-  icon.setAttribute("id", i);
-  btnDelete.setAttribute("id", i);
-  i++;
-
-  btnDelete.appendChild(icon);
-  let groupBtnDelete = document.getElementsByName("btnDelete");
-  let groupIcoDelete = document.getElementsByName("icoDelete");
-
-  btnDelete.addEventListener("click", function (event) {
+            lista = createList(componentes)
+        }
+        
     
-      let z = event.target.id;
-      while (z < groupBtnDelete.length) {
-        groupBtnDelete[z].id--;
-        groupIcoDelete[z].id--;
-        z++;
-      }
-
-    table.deleteRow(event.target.id);
-
-    if(tblBody.childNodes.length == 0){
-        btnClasificar.setAttribute("disabled",true)
-    }
-
-    i = z;
-  });
-
-  let arr = [lista, inputCantidad, btnDelete];
-  addInsert(arr);
-  let test = disableOptionList()
-});
+      let inputCantidad = document.createElement("input");
+      inputCantidad.setAttribute("type", "number");
+    
+      let btnDelete = document.createElement("button");
+      btnDelete.setAttribute("class", "btn btn-danger");
+      btnDelete.setAttribute("name", "btnDelete");
+    
+    
+      let icon = document.createElement("i");
+      icon.setAttribute("class", "bi bi-trash3-fill");
+      icon.setAttribute("name", "icoDelete");
+    
+      icon.setAttribute("id", i);
+      btnDelete.setAttribute("id", i);
+      i++;
+    
+      btnDelete.appendChild(icon);
+      let groupBtnDelete = document.getElementsByName("btnDelete");
+      let groupIcoDelete = document.getElementsByName("icoDelete");
+    
+      btnDelete.addEventListener("click", function (event) {
+        
+          let z = event.target.id;
+          while (z < groupBtnDelete.length) {
+            groupBtnDelete[z].id--;
+            groupIcoDelete[z].id--;
+            z++;
+          }
+    
+        table.deleteRow(event.target.id);
+    
+        if(tblBody.childNodes.length == 0){
+            btnClasificar.setAttribute("disabled",true)
+        }
+    
+        i = z;
+      });
+    
+      let arr = [lista, inputCantidad, btnDelete];
+      addInsert(arr);
+      let test = disableOptionList()
+    });
+}
 
 
 
