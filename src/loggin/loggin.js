e712d538ee5fb67d08da19711648a278fa4a6334
XxSergioLDCxX <119493186+XxSergioLDCxX@users.noreply.github.com>
2023-11-29 14:03:24 +0100

    Redireccion al index

--- src/loggin/loggin.js
+++ src/loggin/loggin.js
@@ -16,7 +16,19 @@ document.getElementById('aceptar').addEventListener('click', function() {
     asyncApiRequest("post", url, bodyContent).then(function(resRegister){
         console.log(resRegister);
         sessionStorage.setItem("token",resRegister.token )
+
+        
+        console.log("sessionStorage.getItem()");
+        if (resRegister.token) {
+            console.log("llego")
+            window.location.href = '../selection-rol/index.html';
+        }else{
+            alert("No se ha podido iniciar sesion");
+        }
         console.log(sessionStorage.getItem("token"));
+
+        
+
     });
     console.log("ADIOS");
 });
