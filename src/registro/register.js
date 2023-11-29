b5fc2c834989f0c9561acd71fe26d504b5e87d7c
XxSergioLDCxX <119493186+XxSergioLDCxX@users.noreply.github.com>
2023-11-29 13:10:17 +0100

    Campo de nombre de empresa

--- src/registro/register.js
+++ src/registro/register.js
@@ -1,6 +1,13 @@
-import {asyncApiRequest} from "../utils/funcs.js"
+import { asyncApiRequest } from "../utils/funcs.js";
+import { domain } from "../utils/funcs.js";
+
 document.addEventListener('DOMContentLoaded', function () {
     console.log("El DOM está completamente cargado y parseado");
+
+    //variables para verificar datos
+    let pass1 = false;
+    let pass2 = false;
+    let pass3 = false;
     
     // Maneja el envío del formulario de registro
     document.getElementById('registerForm').addEventListener('submit', function (e) {
@@ -16,12 +23,30 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('newUsername').addEventListener('input', function () {
     const username = this.value;
     const validationMessage = document.getElementById('usernameValidationMessage');
+    
     if (/^[a-zA-Z0-9_]{4,15}$/.test(username)) {
         validationMessage.textContent = 'Nombre de usuario válido';
         validationMessage.style.color = 'green';
+        pass1 = true
+        
     } else {
         validationMessage.textContent = 'El nombre de usuario debe tener entre 4 y 10 caracteres y solo puede contener letras, números y _';
         validationMessage.style.color = 'red';
+        pass1 = false;
+    }
+});
+
+document.getElementById('newNombreEmpresa').addEventListener('input', function () {
+    const username = this.value;
+    const validationMessage = document.getElementById('nombreEmpresaValidationMessage');
+    if (/^[a-zA-Z0-9_]{3,20}$/.test(username)) {
+        validationMessage.textContent = 'Nombre de empresa válido';
+        validationMessage.style.color = 'green';
+        pass3 = true;
+    } else {
+        validationMessage.textContent = 'El nombre de la empresa debe tener entre 3 y 20 caracteres y solo puede contener letras, números y _';
+        validationMessage.style.color = 'red';
+        pass3 = false; 
     }
 });
 
@@ -46,9 +71,11 @@ document.getElementById('confirmPassword').addEventListener('input', function ()
     if (confirmPassword === password) {
         validationMessage.textContent = 'Las contraseñas coinciden';
         validationMessage.style.color = 'green';
+        pass2 = true;
     } else {
         validationMessage.textContent = 'Las contraseñas no coinciden';
         validationMessage.style.color = 'red';
+        pass2 = false;
     }
 });
 
@@ -59,18 +86,19 @@ document.getElementById('confirmPassword').addEventListener('input', function ()
         const newUsername = document.getElementById('newUsername').value;
         const newPassword = document.getElementById('newPassword').value;
         const newEmail = document.getElementById('newEmail').value;
+        const newNombreEmpresa = document.getElementById('newNombreEmpresa').value
 
         // Realiza la validación final de todos los campos antes de enviar
         const usernameValidationMessage = document.getElementById('usernameValidationMessage');
         const passwordValidationMessage = document.getElementById('passwordValidationMessage');
 
-        if (usernameValidationMessage.style.color === 'green' &&
-            passwordValidationMessage.style.color === 'green') {
+        if (pass1 && pass2 && pass3) {
 
                 alert("dentro");
             // Realiza la petición al servidor
             let bodyContent =JSON.stringify({
                 name: newUsername,
+                name_empresa: newNombreEmpresa,
                 email: newEmail,
                 password: newPassword,
                 }) ;
