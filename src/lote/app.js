b604489b1c4486cb9adbe760b9573deefbec0b2b
bhamidou <badrhamidou@gmail.com>
2023-12-05 00:02:29 +0100

    add feature maps

--- src/lote/app.js
+++ src/lote/app.js
@@ -1,51 +1,59 @@
-import { sendNotification, domain } from "../utils/funcs.js"
+import { sendNotification, domain } from "../utils/funcs.js";
 
-let obs = document.getElementById("obs")
-let ubi = document.getElementById("ubi")
-let btn = document.getElementById("btn")
+let obs = document.getElementById("obs");
+let ubi = document.getElementById("ubi");
+let btn = document.getElementById("btn");
 
-btn.addEventListener('click',function(){
-    btn.setAttribute("disabled","true")
-    let noti = document.getElementById("notification")
+btn.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (ubi.value.length > 1 && obs.value.length > 1) {
+      btn.setAttribute("disabled", "true");
 
-    let loader = document.createElement("div")
-    loader.setAttribute("class","loader")
-    loader.setAttribute("id","loader")
-    noti.appendChild(loader)
+      let noti = document.getElementById("notification");
 
-    let arr = [ubi.value, obs.value, 1]
-    console.log(arr)
-    newLote(arr).then(function(res){
-        document.getElementById("loader").remove()
-        sendNotification(res.status,"alert alert-success")
+      let loader = document.createElement("div");
+      loader.setAttribute("class", "loader");
+      loader.setAttribute("id", "loader");
+      noti.appendChild(loader);
+      
+      let getUserLocal = localStorage.getItem("userId");
+      let arr = [ubi.value, obs.value, getUserLocal];
+      
+
+    newLote(arr)
+      .then(function (res) {
+        document.getElementById("loader").remove();
+        sendNotification(res.status, "alert alert-success");
 
         setTimeout(() => {
-            location.reload();
+          location.reload();
         }, 5000);
+      });
+  } else {
+    sendNotification(
+      "Es obligatorio rellenar ambos campos",
+      "alert alert-danger"
+    );
+  }
+});
 
-    }).catch(function(error){
-        console.log(error)
-    })
+async function newLote(datos) {
+  let headersList = {
+    "Content-Type": "application/json",
+  };
+  let getLocalUbi = localStorage.getItem("ubi");
+  let bodyContent = JSON.stringify({
+    ubi: getLocalUbi,
+    observation: datos[1],
+    user_id: datos[2],
+  });
+  let url = domain + "/api/lote";
+  let response = await fetch(url, {
+    method: "POST",
+    body: bodyContent,
+    headers: headersList,
+  });
 
-})
-async function newLote(datos){
-
-    let headersList = {
-        "Content-Type": "application/json"
-    }
-    let getLocalUbi = localStorage.getItem("ubi")
-    let bodyContent = JSON.stringify({
-        "ubi":getLocalUbi,
-        "observation":datos[1],
-        "user_id":datos[2]
-    });
-    let url = domain+"/api/lote"
-    let response = await fetch(url, { 
-        method: "POST",
-        body: bodyContent,
-        headers: headersList
-    });
-    
-    let data = await response.json();
-    return data
+  let data = await response.json();
+  return data;
 }
