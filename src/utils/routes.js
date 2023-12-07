37ffbd363180514158329cfaa82c38014a98ac92
bhamidou <badrhamidou@gmail.com>
2023-12-07 12:03:07 +0100

    create routes vector for redirections

---
+++ src/utils/routes.js
@@ -0,0 +1,11 @@
+export let rolRoute = {
+    "Colaborador" : "../lote",
+    "Clasificador": "../lote-clasificador",
+    "Diseñador": "../disign",
+    "Admin": "../admin",
+}
+
+export let redirect = {
+    "componentes": "../crear-componente",
+    "info-lote": "../info-lote"
+}
