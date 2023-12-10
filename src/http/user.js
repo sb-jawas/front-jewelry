1701cb2badb17c537fb469a075574e5c7035dc6f
bhamidou <badrhamidou@gmail.com>
2023-12-10 20:26:12 +0100

    add func to updateUser

--- src/http/user.js
+++ src/http/user.js
@@ -26,6 +26,36 @@ export async function asyncUser(methodApi,url, bodyContent){
     }
 }
 
+
+export async function asyncUpdateUser(methodApi,url, bodyContent){ 
+    let headersList = {
+        "Content-Type": "application/json",
+        "Authorization" : "Bearer " + getUserToken()
+    };
+    try{
+        let response = await fetch(url, {
+            method: methodApi,
+            body: bodyContent,
+            headers: headersList,
+        });
+        
+        if(response.ok==400){
+            return response.status
+        }
+        if (!response.ok) {
+            let errorResponse = await response.text();
+            return errorResponse
+        } else {
+            let data = await response.json();
+            return data;
+        }
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+
 export async function loginUser(){
 
 }
