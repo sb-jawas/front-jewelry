b7a53cd50d19c36abed02c510e82bbbc8b5cd4fa
bhamidou <badrhamidou@gmail.com>
2023-12-11 23:34:00 +0100

    fixing id for select option

--- src/selection-rol/app.js
+++ src/selection-rol/app.js
@@ -10,13 +10,14 @@ let method = "GET"
 
 asyncApiRequest(method, url).then(function(resData){
     if(resData.length==1){
+        localStorage.setItem("rolId",resData[0].id)
         window.location.href = rolRoute[resData[0].name]
     }else{
         let i = 0
         console.log(rolRoute[resData[0].name])
         while(i<resData.length){
             let option = document.createElement("option")
-            option.id = resData.length-i
+            option.id = resData[i].id
             option.text = resData[i].name
             option.href = rolRoute[resData[i].name]
             lista.appendChild(option)
