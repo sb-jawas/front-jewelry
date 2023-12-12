d2af05a2424ee6978d485978948a7d184b6cc67f
bhamidou <badrhamidou@gmail.com>
2023-12-12 14:23:59 +0100

    update conf webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,32 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
+    users: './src/admin-users/app.js',
+    crearComponentes: './src/crear-componente/app.js',
+    forgetPass: './src/forget-pass/app.js',
+    homeAdmin: './src/home-admin/app.js',
+    homeDesigner: './src/home-designer/app.js',
+    homeLote: './src/home-lote/app.js',
+    users: './src/admin-users/app.js',
+    infoLote: './src/info-lote/app.js',
     lote: './src/lote/app.js',
-    clasificador:'./src/lote-clasificador/app.js',
-    mislotes: './src/mis-lotes/app.js',
-    infolote: './src/info-lote/app.js'
+    loteClasificador:'./src/lote-clasificador/app.js',
+    loteHistorial: './src/lote-historial/app.js',
+    misComponentes: './src/mis-componentes/app.js',
+    misLotes: './src/mis-lotes/app.js',
+    profile: './src/my-profile/app.js',
+    signup: './src/signup/app.js',
+    login: './src/app.js',
+  
  },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, './src/dist'),
+    path: path.resolve(__dirname, './dist'),
     // clean: true,
   },
   devServer: {
     static: path.resolve(__dirname,"./"),
-    port: 5555,
+    port: 3000,
     open: {
       target: "./src/"
     },
