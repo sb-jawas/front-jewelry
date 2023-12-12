a908ab0e059925f83b933ae81722638912e546d3
bhamidou <badrhamidou@gmail.com>
2023-12-12 17:11:04 +0100

    home view for designer

---
+++ src/home-designer/app.js
@@ -0,0 +1,6 @@
+import { getLocalStorage, redirectToMyRol } from "../utils/funcs.js";
+import { navbar } from "../utils/navbar.js";
+
+let rolView = 3
+redirectToMyRol(rolView)
+navbar(getLocalStorage("userName"),"Colaborador")
